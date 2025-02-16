b9133f880fc13781af5f9e297cabdbe273722519
iLabGlobalTek <kylek@ilabantigua.com>
2025-02-16 17:00:07 -0400

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/stits-app-react/", // Set this to match your GitHub Pages repo name
   plugins: [react()],
+  base: "/stits-app-react/", // Set this to match your GitHub Pages repo name
 });
